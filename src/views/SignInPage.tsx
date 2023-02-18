8c6889e642e82b5b2c6ed79cbde1c302a848fdb9
harrisking <llc13588440410@outlook.com>
2023-02-18 13:12:06 +0800

    feat: 面试自动登录

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -25,6 +25,13 @@ export const SignInPage = defineComponent({
     const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
     const router = useRouter()
     const route = useRoute()
+
+    if (route.query.preview === 'yes') {
+      formData.email = 'liliancheng33@foxmail.com'
+      formData.code = '123456'
+
+    }
+
     const onSubmit = async (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -36,7 +43,7 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData, {_autoLoading: true})
+        const response = await http.post<{ jwt: string }>('/session', formData, { _autoLoading: true })
           .catch(onError)
         localStorage.setItem('jwt', response.data.jwt)
         const returnTo = route.query.return_to?.toString()
@@ -54,7 +61,7 @@ export const SignInPage = defineComponent({
 
       disabled()
       await http
-        .post('/validation_codes', { email: formData.email } , {
+        .post('/validation_codes', { email: formData.email }, {
           _autoLoading: true
         })
         .catch(onError)
